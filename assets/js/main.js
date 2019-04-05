1362a1e2441603a58f2b48892b0a065292c232db
Kanjamad Bosel <npor25@yahoo.com>
2019-04-05 16:44:07 -0700

    add jquery submit form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,13 @@
 
 console.log('Hi!!!!!');
 
-    $(document).ready(function () {
-    
-        $("button").click(function(event) {
-            event.preventDefault();
-            console.log('click!!!!!!!!!!!!');
-            // console.log($(this).parent())
-            $('.fas').animate({left: '550px'}, 4000);
-        });
-
-        $("form").click(function(event) {
-            event.preventDefault();
-            console.log("submit!!!!!!!!!!!!");
-            $(this).replaceWith('<h1>Thank you for submit the form..........</h1>');
-        });
-
-    });
-
-
-
-
-
-
     // $(document).ready(function () {
     
-    //     $( 'button' ).on('click', function(event) {
+    //     $("button").click(function(event) {
     //         event.preventDefault();
     //         console.log('click!!!!!!!!!!!!');
-    //         console.log($(this).parent())
-    //     // $(this).parent().find('.fas').animate({left: '550px'}, 4000);
+    //         // console.log($(this).parent())
+    //         $('.fas').animate({left: '550px'}, 4000);
     //     });
 
     //     $("form").click(function(event) {
@@ -39,3 +17,27 @@ console.log('Hi!!!!!');
     //     });
 
     // });
+
+
+
+
+
+
+    $(document).ready(function () {
+    
+        $('form').on('click', function(event) {
+            event.preventDefault();
+            console.log('click!!!!!!!!!!!!');
+            $('#fas1').animate({left: '550px'}, 4000);
+            $(this).replaceWith('<h1>Thank you for submit the form..........</h1>');
+        });
+
+
+        $('.projectHeader').click(function(event) {
+            event.preventDefault();
+            console.log("click project!!!!!!!!!!!!");
+            $('.fa-car-side').animate({left: '550px'}, 4000);
+
+        });
+
+    });
